b66a95c1144e2a5cc542c867749a13023e33fbee
Akemi-Homura <770649183@qq.com>
2018-09-30 16:36:38 +0800

    complete L-8-5

---
+++ Chapter8/L-8-5/a.c
@@ -0,0 +1,72 @@
+# include <stdio.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+void pr_exit(int status);
+
+void
+pr_exit(int status){
+    if (WIFEXITED(status)){
+        printf("normal terminatin, exit status = %d\n",
+                WEXITSTATUS(status));
+    }else if (WIFSIGNALED(status)){
+        printf("abnormal termination, singal number = %d%s\n",
+                WTERMSIG(status),
+#ifdef  WCOREDUMP
+                WCOREDUMP(status) ? " (core file generated)" : "");
+#else
+                "");
+#endif
+    }else if (WIFSTOPPED(status)){
+        printf("child stopped, signal number = %d\n",
+                WSTOPSIG(status));
+    }
+}
+
+int
+main(void){
+    pid_t   pid;
+    int     status;
+
+    if ((pid = fork()) < 0){
+        perror("fork error");
+        exit(1);
+    }else if (pid == 0){    /* child */
+        exit(7);
+    }
+
+    if (wait(&status) != pid){ /* wait for child */
+        perror("wait error");
+        exit(1);
+    }
+    pr_exit(status); /* and print its status */
+
+    if ((pid = fork()) < 0){
+        perror("fork error");
+        exit(1);
+    }else if( pid == 0){ /* child */
+        abort();        /* generates SIGABRT */
+    }
+
+    if (wait(&status) != pid){ /* wait for child */
+        perror("wait error");
+        exit(1);
+    }
+    pr_exit(status);
+
+    if ((pid = fork()) < 0){
+        perror("fork error");
+        exit(1);
+    }else if( pid == 0){ /* child */
+        status /= 0;    /* divide by 0 generates SIGFPE */
+    }
+
+    if (wait(&status) != pid){ /* wait for child */
+        perror("wait error");
+        exit(1);
+    }
+    pr_exit(status);
+
+    exit(0);
+}
