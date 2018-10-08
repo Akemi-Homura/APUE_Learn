43805c3fe39c51bd14cd64a78c84aa7136317f47
Akemi-Homura <770649183@qq.com>
2018-10-08 17:04:41 +0800

    complete L-10-5

---
+++ Chapter10/L-10-5/a.c
@@ -0,0 +1,37 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <pwd.h>
+
+static void
+my_alarm(int signo){
+    struct passwd *rootptr;
+
+    printf("in signal handler\n");
+    if ((rootptr = getpwnam("root")) == NULL){
+        perror("getpwnam(root) error");
+        exit(1);
+    }
+    alarm(1);
+}
+
+int
+main(void){
+    struct passwd *ptr;
+
+    signal(SIGALRM, my_alarm);
+    alarm(1);
+    while(1){
+        if ((ptr = getpwnam("quals")) == NULL){
+            perror("getpwnam errror");
+            exit(1);
+        }
+        if (strcmp(ptr->pw_name, "quals") != 0){
+            printf("return value corrupted!, pw_name = %s\n",
+                    ptr->pw_name);
+        }
+    }
+}
